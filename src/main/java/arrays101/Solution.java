eb7611564b6e9a011a34ad44e7d21448399892e7
Olex1313 <aa1limonov@gmail.com>
2022-04-27 23:17:09 +0300

    :feature: - add a couple of new tasks

--- src/main/java/arrays101/Solution.java
+++ src/main/java/arrays101/Solution.java
@@ -2,12 +2,17 @@ package arrays101;
 
 import utils.NumberUtils;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
 
     /**
-     * @see <a href="https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3238/">Task</a>
      * @param nums array of integers with value 0 or 1
      * @return maximum number of consecutive 1's in array
+     * @see <a href="https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3238/">Task</a>
      */
     public static int findMaxConsecutiveOnes(int[] nums) {
         int buffer = 0;
@@ -25,9 +30,9 @@ class Solution {
     }
 
     /**
-     * @see <a href="https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3237/">Task</a>
      * @param nums array of integers in between 1..10^5
      * @return amount of numbers with an even number of digits
+     * @see <a href="https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3237/">Task</a>
      */
     public static int findNumbers(int[] nums) {
         if (nums.length == 0) {
@@ -42,5 +47,62 @@ class Solution {
         return count;
     }
 
+    /**
+     * @param array array of integers in 0..9
+     *              Duplicates zeroes in array
+     * @see <a href="https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/">Task</a>
+     */
+    public static void duplicateZeroes(int[] array) {
+        boolean lastChanged = false;
+        for (int i = 0; i < array.length - 1; ) {
+            if (array[i] == 0) {
+                for (int j = array.length - 1; j > i + 1; j--) {
+                    array[j] = array[j - 1];
+                }
+                array[i + 1] = 0;
+                lastChanged = true;
+            }
+            i += (lastChanged) ? 2 : 1;
+            lastChanged = false;
+        }
+    }
+
+    /**
+     * @param nums array of integers in 0..50 with nums.length in 0..100
+     * @param val  value to remove
+     * @return number of elements left in array
+     * Removes val from array
+     * @see <a href="https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/">Task</a>
+     */
+    public static int removeElement(int[] nums, int val) {
+        int j = 0; // pointer to element in "new" array
+        for (int i = 0; i < nums.length && j < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    /**
+     * @param arr array of integers in 10^-3..10^3 with length in 2..500
+     * @return Checks if there exists two indices i and j such that i != j
+     * and arr[i] == 2 * arr[j]
+     * @see <a href="https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3250/">Task</a>
+     */
+    public static boolean checkIf(int[] arr) {
+        Set<Integer> numToIndex = new HashSet<>();
+        for (int num : arr) {
+            if (numToIndex.contains(num * 2)) {
+                return true;
+            }
+            if (numToIndex.contains(num / 2) && num % 2 == 0) {
+                return true;
+            }
+            numToIndex.add(num);
+        }
+        return false;
+    }
 
 }
--- src/test/java/arrays101/SolutionTest.java
+++ src/test/java/arrays101/SolutionTest.java
@@ -1,11 +1,14 @@
 package arrays101;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
@@ -29,6 +32,45 @@ class SolutionTest {
 
         // when
         int actualAnswer = Solution.findNumbers(nums);
+
+        // then
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @ParameterizedTest
+    @MethodSource("duplicateZeroesProvider")
+    void duplicateZeroes(int[] givenArray, int[] expectedAnswer) {
+        // given
+
+        // when
+        Solution.duplicateZeroes(givenArray);
+
+        // then
+        assertArrayEquals(expectedAnswer, givenArray);
+    }
+
+    @ParameterizedTest
+    @MethodSource("removeElementProvider")
+    void removeElement(int[] nums, int val, int[] expectedNums, int expectedAnswer) {
+        // given
+
+        // when
+        int answer = Solution.removeElement(nums, val);
+
+        // then
+        assertEquals(expectedAnswer, answer);
+        for(int i = 0; i < expectedAnswer; i++) {
+            assertEquals(expectedNums[i], nums[i]);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("checkIfProvider")
+    void checkIf(int[] arr, boolean expectedAnswer) {
+        // given
+
+        // when
+        boolean actualAnswer = Solution.checkIf(arr);
 
         // then
         assertEquals(expectedAnswer, actualAnswer);
@@ -61,4 +103,49 @@ class SolutionTest {
                 Arguments.of(fourthCase, 1)
         );
     }
+
+    public static List<Arguments> duplicateZeroesProvider() {
+        int[] firstCase = {1, 0, 2, 3, 0, 4, 5, 0};
+        int[] firstCaseAnswer = {1, 0, 0, 2, 3, 0, 0, 4};
+        int[] secondCase = {1, 2, 3};
+        int[] secondCaseAnswer = {1, 2, 3};
+        int[] thirdCase = {0, 0, 0};
+        int[] thirdCaseAnswer = {0, 0, 0};
+        int[] fourthCase = {1, 0, 2};
+        int[] fourthCaseAnswer = {1, 0, 0};
+        int[] fifthCase = {0, 4, 1, 0, 0, 8, 0, 0, 3};
+        int[] fifthCaseAnswer = {0, 0, 4, 1, 0, 0, 0, 0, 8};
+        return List.of(
+                Arguments.of(firstCase, firstCaseAnswer),
+                Arguments.of(secondCase, secondCaseAnswer),
+                Arguments.of(thirdCase, thirdCaseAnswer),
+                Arguments.of(fourthCase, fourthCaseAnswer),
+                Arguments.of(fifthCase, fifthCaseAnswer)
+        );
+    }
+
+    public static List<Arguments> removeElementProvider() {
+        int[] firstCase = {1, 0, 0, 2, 3};
+        int[] firstCaseAnswer = {1, 2, 3, 0, 0};
+        int[] secondCase = {1, 2, 3};
+        int[] secondCaseAnswer = {1, 3, 3};
+        return List.of(
+                Arguments.of(firstCase, 0, firstCaseAnswer, 3),
+                Arguments.of(secondCase, 2, secondCaseAnswer, 2)
+        );
+    }
+
+    public static List<Arguments> checkIfProvider() {
+        int[] firstCase = {10, 2, 5, 3};
+        int[] secondCase = {7, 1, 14, 11};
+        int[] thirdCase = {3, 1, 7, 11};
+        int[] fourthCase = {0, 1};
+        return List.of(
+                Arguments.of(firstCase, true),
+                Arguments.of(secondCase, true),
+                Arguments.of(thirdCase, false),
+                Arguments.of(fourthCase, false)
+        );
+    }
+
 }
