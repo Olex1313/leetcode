2ac8bf842d14f738c8cec13c6c055b1a03465234
a.a.limonov <a.a.limonov@tinkoff.ru>
2023-03-13 09:35:29 +0300

    SymmetricTree leetcode daily

---
+++ src/main/java/easy/SymmetricTree.java
@@ -0,0 +1,42 @@
+package easy;
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSym(root.left, root.right);
+    }
+
+    private boolean isSym(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSym(left.left, right.right) && isSym(left.right, right.left);
+    }
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
